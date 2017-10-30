ac06e670ab1102bff9ddccc4fb0ca8489d4f55e0
Misha Holtz <mishmish@eduroam-169-233-209-212.ucsc.edu>
2017-10-30 14:33:07 -0700

    Initial Commit

---
+++ Conifr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conifr
+//
+//  Created by Misha Holtz on 10/30/17.
+//  Copyright © 2017 Conifr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
